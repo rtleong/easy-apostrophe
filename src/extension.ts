1ae6e55b63f6ba0ee39579aab3ab9413cad955e3
Riley <rileyleong24@gmail.com>
2024-07-21 16:15:26 -0700

    Initial development of easy apostrophe, program is functional but has bugs where it will single-quote the entire line and not delineate by = or <-

--- src/extension.ts
+++ src/extension.ts
@@ -5,21 +5,34 @@ import * as vscode from 'vscode';
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+    let disposable = vscode.commands.registerCommand('extension.easyapostrophe', () => {
+        const editor = vscode.window.activeTextEditor;
+        if (editor) {
+            const document = editor.document;
+            const selection = editor.selection;
+            const position = selection.active;
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "easyapostrophe" is now active!');
+            // Get the current line
+            const lineText = document.lineAt(position.line).text;
+            const lineLength = lineText.length;
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('easyapostrophe.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from EasyApostrophe!');
-	});
+            // Check if the cursor is at the end of the string
+            if (position.character === lineLength) {
+                // Find the first non-whitespace character
+                const firstNonWhitespaceIndex = lineText.search(/\S/);
 
-	context.subscriptions.push(disposable);
+                if (firstNonWhitespaceIndex !== -1) {
+                    editor.edit(editBuilder => {
+                        // Insert apostrophe at the start and end of the string
+                        editBuilder.insert(new vscode.Position(position.line, firstNonWhitespaceIndex), "'");
+                        editBuilder.insert(position, "'");
+                    });
+                }
+            }
+        }
+    });
+
+    context.subscriptions.push(disposable);
 }
 
 // This method is called when your extension is deactivated
